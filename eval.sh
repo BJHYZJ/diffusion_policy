218e0c4fdf682c6fac67aa7bebb0f724c796b2d1
Zhijie Yan <yanzhijie@buaa.edu.cn>
2026-01-17 13:17:22 +0800

    update

---
+++ eval.sh
@@ -0,0 +1,4 @@
+python eval.py \
+    --checkpoint data/experiments/low_dim/pusht/diffusion_policy_cnn/train_0/checkpoints/epoch=0550-test_mean_score=0.969.ckpt \
+    --output_dir data/pusht_eval_output \
+    --device cuda:7
